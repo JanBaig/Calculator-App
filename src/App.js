5ea7282effcf0c7f61ce5c7af6415995111b297c
Janat <janatbaig2@gmail.com>
2022-04-05 20:48:44 -0400

    First Features Implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
+
 function App() {
+  const [input, setInput] = useState("");
+  
+
+  function DisplayButtons(){
+
+    // Make an Array of buttons 
+    var divArray = [];
+
+    for (var i=0; i<15; i++) {
+      if (i == 9){
+        divArray.push(
+        <div key={i+1}>
+          <button onClick={() => setInput("0")} className="buttons">0</button>
+        </div>)
+      }
+      else if (i == 10){
+        divArray.push(
+        <div key={i+1}>
+          <button onClick={() => setInput(".")} className="buttons">.</button>
+        </div>)
+      }
+      else if (i == 11){
+        divArray.push(
+        <div key={i+1}>
+          <button onClick={() => setInput("")}className="buttons">=</button>
+        </div>)
+      }
+      else if (i == 12){
+        divArray.push(
+        <div key={i+1}>
+          <button onClick={() => setInput("+")}className="buttons">+</button>
+        </div>)
+      }
+      else if (i == 13){
+        divArray.push(
+        <div key={i+1}>
+          <button onClick={() => setInput("x")}className="buttons">x</button>
+        </div>)
+      }
+      else if (i == 14){
+        divArray.push(
+        <div key={i+1}>
+          <button onClick={() => setInput("/")} className="buttons">÷</button>
+          </div>)
+      }
+      else {
+        var tempState = [...input + i];
+        divArray.push(
+        <div key={i+1}>
+          <button onClick={() => setInput(tempState)}className="buttons">{i+1}</button>
+        </div>)
+      }
+
+    }
+    
+
+    return (
+      <div className="gridContainer">
+        {divArray}
+        <button className="clearBtn">CLEAR</button>
+      </div>
+      
+    )
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div >
+
+      <h2>Simple Calculator</h2>
+
+      <div className="outerBox">
+
+          <div className="header">
+            <div className="displayBar">
+              <p>{input}</p>
+            </div>
+          </div>
+
+          <div className="body">
+
+            <DisplayButtons />
+
+          </div>
+
+      </div>
+
+      
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+// Making a Simple Calculator App
+
+// Create this app without following any tutorials. Just brute force your way to get something working. At the end, watch a tutorial to see how others more experienced structured thier applications. 
---
+++ src/Components/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Button({index}){
+
+    // return (
+    //     <div key={index+1}>
+    //         <button onClick={() => setInput(input)} className="buttons"> {index+1} </button>
+    //     </div>
+    // )
+
+
+
+}
+
+export default Button;
